44e12a03c01ddcfde923d08a4896726b74dbcd00
Salience100 <bhardwajshashank1506@gmail.com>
2024-05-05 23:19:22 -0400

    Added additional exception

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -10,7 +10,7 @@ class TestQuizFunctionality(unittest.TestCase):  # Changed 'QuizzesTest' to 'Tes
         # Setting up the environment for testing with non-production data
         self.quiz_ctrl = QuizzesController('quizzes_test.py')  # Changed 'ctrl' to 'quiz_ctrl'
 
-    # The error occurs at line 45 of "quizzes_test.py"
+    # The error occurs at line 94 of "quizzes_controller.py"
     def test_failure_case_01(self):
         # Initializing input parameters for add_quiz function
         title = "Quiz Title"
@@ -19,30 +19,32 @@ class TestQuizFunctionality(unittest.TestCase):  # Changed 'QuizzesTest' to 'Tes
         due_datetime = datetime(2024, 5, 3, 11, 11, 11)
 
         # Fetch quiz_id from add_quiz function
-        quiz_id = self.quiz_ctrl\
+        quiz_id = self.quiz_ctrl \
             .add_quiz(title=title,
                       text=text,
                       available_date=available_datetime,
                       due_date=due_datetime)
 
         # Fetch question_id from add_question function
-        question_id = self.quiz_ctrl\
+        question_id = self.quiz_ctrl \
             .add_question(quiz_id,
                           title=title,
                           text=text)
 
-        # Initializing is_correct with a string value instead of expected boolean type
-        is_correct = "Passing a string in place of expected boolean datatype"
+        # Initializing input parametes for add_answer function
+        is_correct = True
+        # Initializing text parameter with a datetime value instead of expected string type
+        answer_text = datetime.now()
 
         # Fetching answer_id from add_answer function
-        # answer_id should be null because of passing string instead of boolean datatype
-        answer_id = self.quiz_ctrl\
-            .add_answer(question_id,
-                        text,
-                        is_correct)
+        # Throws TypeError as datetime was passed instead of expected string
+        answer_id = self.quiz_ctrl \
+            .add_answer(question_id=question_id,
+                        text=answer_text,
+                        is_correct=is_correct)
 
         # Assertion should fail as answer_id is None
-        self.assertIsNone(answer_id, "answer_id is None")
+        self.assertIsNone(answer_id, "answer_id is expected to be None")
 
     # The error occurs at line 81 of quiz_controller.py
     def test_failure_case_02(self):
@@ -53,7 +55,7 @@ class TestQuizFunctionality(unittest.TestCase):  # Changed 'QuizzesTest' to 'Tes
         due_datetime = datetime(2024, 5, 3, 11, 11, 11)
 
         # Fetch quiz_id from add_quiz function
-        quiz_id = self.quiz_ctrl\
+        quiz_id = self.quiz_ctrl \
             .add_quiz(title=title,
                       text=text,
                       available_date=available_datetime,
@@ -65,7 +67,7 @@ class TestQuizFunctionality(unittest.TestCase):  # Changed 'QuizzesTest' to 'Tes
 
         # Fetch question_id from add_question function
         # Throws TypeError as title is not Serializable
-        question_id = self.quiz_ctrl\
+        question_id = self.quiz_ctrl \
             .add_question(quiz_id=quiz_id,
                           title=title,
                           text=text)
@@ -75,7 +77,6 @@ class TestQuizFunctionality(unittest.TestCase):  # Changed 'QuizzesTest' to 'Tes
 
     # The error occurs at line 63 of quiz_controller.py
     def test_failure_case_03(self):
-
         # Initializing input parameters for add_quiz function
         # Initializing title with integer value instead of expected string value
         title = 1
@@ -84,8 +85,8 @@ class TestQuizFunctionality(unittest.TestCase):  # Changed 'QuizzesTest' to 'Tes
         due_datetime = datetime(2024, 5, 3, 11, 11, 11)
 
         # Fetch quiz_id from add_quiz function
-        # Should throw unsupported operand type exception
-        quiz_id = self.quiz_ctrl\
+        # Throws unsupported operand type exception
+        quiz_id = self.quiz_ctrl \
             .add_quiz(title=title,
                       text=text,
                       available_date=available_datetime,
